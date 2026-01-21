ec745ccaf7288a82391312e89912e2cbbb916619
mmiral04 <mariomiralles04@gmail.com>
2026-01-21 13:53:45 +0100

    barbar plugin for neovim

--- nvim/lua/meide/core/keymaps.lua
+++ nvim/lua/meide/core/keymaps.lua
@@ -94,3 +94,21 @@ vim.keymap.set("n", "<leader>lx", function()
         underline = isLspDiagnosticsVisible
     })
 end, { desc = "Toggle LSP diagnostics" })
+
+-- barbar mappings
+vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+
+vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+vim.keymap.set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+vim.keymap.set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+vim.keymap.set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+vim.keymap.set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+vim.keymap.set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+vim.keymap.set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+vim.keymap.set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+vim.keymap.set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
+vim.keymap.set('n', '<leader>bp',   '<Cmd>BufferPick<CR>', opts)
+vim.keymap.set('n', '<leader>bd', '<Cmd>BufferPickDelete<CR>', opts)
